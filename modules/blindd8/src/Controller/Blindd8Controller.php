7b1d061fb4ce8e3c56f1652293a43ce05037528a
Lubomir Culen <mirsoft@gmx.net>
2016-05-19 14:26:26 +0200

    Subrequests - not working

--- modules/blindd8/src/Controller/Blindd8Controller.php
+++ modules/blindd8/src/Controller/Blindd8Controller.php
@@ -10,6 +10,7 @@ namespace Drupal\blindd8\Controller;
 use Drupal\blindd8\BlindD8NotableEvent;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 /**
  * Defines a controller to experiment with.
@@ -45,8 +46,14 @@ class Blindd8Controller extends ControllerBase
     $blindd8ingservice = \Drupal::service('blindd8.blindd8ingservice');
     $tagline = $blindd8ingservice->getTagline();
 
+    // Let's create a subrequest
+    $request = \Drupal::service('request_stack');
+    $subrequest = $request::create($request->getBaseUrl() . '/admin', 'GET', [], $request->cookies->all(), [], $request->server->all());
+    $response = \Drupal::service('http_kernel')->handle($subrequest, HttpKernelInterface::SUB_REQUEST);
+
     $output = array(
-      '#markup' => $this->t('Hey @name, here is a unique ID for you: @uuid. @tagline @string', ['@name' => $name, '@uuid' => $uuid, '@tagline' => $tagline, '@string' => $string]),
+//      '#markup' => $this->t('Hey @name, here is a unique ID for you: @uuid. @tagline @string', ['@name' => $name, '@uuid' => $uuid, '@tagline' => $tagline, '@string' => $string]),
+      '#markup' => $response->getContent()
     );
     return $output;
   }
